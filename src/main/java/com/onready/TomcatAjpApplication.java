acc817d45688aa66a6e19aa74d29b5b74105684b
onready <desarrollo@onready.com.ar>
2016-12-27 18:58:13 -0300

    Commit inicial

---
+++ src/main/java/com/onready/TomcatAjpApplication.java
@@ -0,0 +1,12 @@
+package com.onready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatAjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatAjpApplication.class, args);
+	}
+}
